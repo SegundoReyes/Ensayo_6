47a055c65d502bec408b3fa92fe2ceafb83df891
bboyc <bboyc@DESKTOP-N0RDKUL>
2024-08-25 22:12:32 -0500

    Netbeans ejemplo

---
+++ src/ensayo_6/Ensayo_6.java
@@ -0,0 +1,16 @@
+
+package ensayo_6;
+import java.util.Scanner;  
+import java.lang.Math;
+public class Ensayo_6 {  
+public static void main(String[] args) {
+double diagonalMayor, diagonalMenor, area;
+  Scanner lectura = new Scanner(System.in);
+  System.out.print("Ingrese la diagonal mayor (en cm): ");
+  diagonalMayor = lectura.nextDouble();
+  System.out.print("Ingrese la diagonal menor (en cm): ");
+  diagonalMenor = lectura.nextDouble();
+  area = (diagonalMayor * diagonalMenor) / 2;
+  System.out.println("El área del rombo es: " + area + " cm²");       
+    } 
+}
